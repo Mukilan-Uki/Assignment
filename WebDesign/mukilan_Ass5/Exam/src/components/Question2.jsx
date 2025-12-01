1c66c0e7b755d2966c09a26175a74f16976295fa
Mukilan-Uki <mukilan.uki.stu@dreamspace.academy>
2025-12-01 15:57:56 +0530

    web design

---
+++ WebDesign/mukilan_Ass5/Exam/src/components/Question2.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+export default function Question2(){
+    const [data,setData]=useState({
+        title:"",
+        price:""
+    })
+
+    let handleChange(e)=>{
+        const {name,value}=e.target
+
+        setData(prevData=>({
+            ...prevData,[name]:value
+    }))
+    }
+
+    const [formData,setFormData]=useState("");
+
+    let handleSubmit(e)=>{
+        e.preventDefault();
+
+        setFormData(data)
+    }
+
+    return(
+        <>
+        <h1>Expense Tracker</h1>
+        <input type="text" value={setData.title} name="title" onChange={handleChange} />
+        <input type="number" value={setData.price} name="price" onChange={handleChange} />
+        <button onClick={handleSubmit}>Add Expense</button>
+
+        {formData &&(
+            <ul>
+                <li>{formData.title}-{formData.price}</li>
+            </ul>
+        )}
+        </>
+    )
+}
---
+++ WebDesign/mukilan_Ass5/project/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Question2 from './components/Question2';
+
+function App() {
+  return (
+    <>
+    <Question2></Question2>
+    </>
+  );
+}
+
+export default App;
